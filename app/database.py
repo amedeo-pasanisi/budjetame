597b618ba34cad3b73972b23ec6ae4d25b8a855b
Amedeo Pasanisi <a.pasanisi@elis.org>
2025-07-07 00:00:29 +0200

    created movement crud endpoints

--- app/database.py
+++ app/database.py
@@ -1,5 +1,14 @@
-from sqlmodel import create_engine
+from typing import Annotated
+from fastapi import Depends
+from sqlmodel import Session, create_engine
 from app.config import config
 
 db_url = f"{config.db_dialect}://{config.db_user}:{config.db_password}@{config.db_host}:{config.db_port}/{config.db_name}"
 engine = create_engine(db_url, echo=True)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDependency = Annotated[Session, Depends(get_session)]
+
--- app/routers/movements.py
+++ app/routers/movements.py
@@ -1,54 +1,64 @@
-from typing import List
 from uuid import UUID
-from fastapi import APIRouter
-from app.models import Movement
+from fastapi import APIRouter, HTTPException, Query
+from app.database import SessionDependency
+from app.models import Movement, MovementCreate, MovementRead, MovementUpdate
+from sqlmodel import select
 
 router = APIRouter(
     prefix="/movements",
     tags=["movements"]
 )
 
-movements: list[Movement] = []
-
 
 @router.get(
     "/",
-    response_model=List[Movement]
+    response_model=list[MovementRead]
 )
-async def read_movements():
+async def read_movements(session: SessionDependency, offset: int = 0, limit: int = Query(default=100, le=100)):
+    movements = session.exec(select(Movement).offset(offset).limit(limit)).all()
     return movements
 
 @router.post(
     "/",
-    response_model=Movement
+    response_model=MovementRead
 )
-async def create_movement(payload: Movement):
-    movements.append(payload)
-    return payload
+async def create_movement(payload: MovementCreate, session: SessionDependency):
+    db_movement = Movement.model_validate(payload)
+    session.add(db_movement)
+    session.commit()
+    session.refresh(db_movement)
+    return db_movement
 
 @router.get(
     "/{movement_id}",
-    response_model=Movement
+    response_model=MovementRead
 )
-async def read_movement(movement_id: UUID):
-    for movement in movements:
-        if movement.id == movement_id:
-            return movement
+async def read_movement(movement_id: UUID, session: SessionDependency):
+    movement = session.get(Movement, movement_id)
+    if not movement:
+        raise HTTPException(status_code=404, detail="Movement not found")
+    return movement
 
 @router.patch(
     "/{movement_id}",
-    response_model=Movement
+    response_model=MovementRead
 )
-async def update_movement(movement_id: UUID, payload: Movement):
-    for idx, movement in enumerate(movements):
-        if movement_id == movement.id:
-            movements[idx] = payload
-            return payload
+async def update_movement(movement_id: UUID, payload: MovementUpdate, session: SessionDependency):
+    db_movement = session.get(Movement, movement_id)
+    if not db_movement:
+        raise HTTPException(status_code=404, detail="Movement not found")
+    movement_data = payload.model_dump(exclude_unset=True)
+    db_movement.sqlmodel_update(movement_data)
+    session.add(db_movement)
+    session.commit()
+    session.refresh(db_movement)
+    return db_movement
 
-@router.delete(
-    "/{movement_id}",
-)
-async def delete_movement(movement_id: UUID):
-    for idx, movement in enumerate(movements):
-        if movement_id == movement.id:
-            movements.pop(idx)
+@router.delete("/{movement_id}")
+async def delete_movement(movement_id: UUID, session: SessionDependency):
+    movement = session.get(Movement, movement_id)
+    if not movement:
+        raise HTTPException(status_code=404, detail="Movement not found")
+    session.delete(movement)
+    session.commit()
+    return {"ok": True}
