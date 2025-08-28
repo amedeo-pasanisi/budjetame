e7cb89bfbf99340c33535645c7b4a5fd165e3b7c
Amedeo Pasanisi <a.pasanisi@elis.org>
2025-08-28 22:03:35 +0200

    added CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from app.config import get_settings
 from app.routers import movements
+from fastapi.middleware.cors import CORSMiddleware
+
+settings=get_settings()
 
 app = FastAPI(
     title="Budjetame",
     openapi_url="/openapi.json",
     swagger_ui_parameters={"docExpansion": "none"}
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(movements.router)
