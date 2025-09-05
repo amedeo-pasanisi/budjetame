cf3297d8564cf1245fb4d36d15311065ec09c7c9
Amedeo Pasanisi <a.pasanisi@elis.org>
2025-09-05 23:23:02 +0200

    added type column to movement model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timezone
+from typing import Literal
 from uuid import UUID, uuid4
 from pydantic import ConfigDict, alias_generators
-from sqlmodel import SQLModel, Field
-
+from sqlmodel import SQLModel, Field, String
 
 class MovementBase(SQLModel):
     model_config = ConfigDict(
@@ -11,6 +11,7 @@ class MovementBase(SQLModel):
     )
     amount: float = Field(description="Amount of euros. Positive if gained, negative if lost.")
     description: str | None = Field(default=None, description="How or why the amount was spent or gained.")
+    type: Literal["gain", "loss", "movement"] = Field(sa_type=String, default="loss")
 
 class Movement(MovementBase, table=True):
     id: UUID | None = Field(default_factory=uuid4, primary_key=True)
@@ -27,6 +28,7 @@ class MovementUpdate(SQLModel):
     )
     amount: float | None = Field(default=None, description="Amount of euros. Positive if gained, negative if lost.")
     description: str | None = Field(default=None, description="How or why the amount was spent or gained.")
+    type: Literal["gain", "loss", "movement"] | None = Field(default=None)
 
 class MovementRead(MovementBase):
     id: UUID
